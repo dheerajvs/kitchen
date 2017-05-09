ad940847f804a3022dd1e52d4d67a669aa1a8a9b
Dheeraj <dheerajvs@gmail.com>
2017-05-09 23:54:11 +0530

    use base tag for base urls

--- build.js
+++ build.js
@@ -2,10 +2,17 @@ const fs = require('fs');
 const Yaml = require('yamljs');
 const Handlebars = require('handlebars');
 
+const recipeData = Yaml.load('html/data/recipes.yml');
+// fs.mkdirSync('public/recipe');
+
+Handlebars.registerPartial('body', fs.readFileSync('html/pages/index.html', 'utf8'));
+const indexTemplate = Handlebars.compile(fs.readFileSync('html/layouts/default.hbs', 'utf8'));
+fs.writeFileSync('public/index.html', indexTemplate(recipeData));
+
 Handlebars.registerPartial('body', fs.readFileSync('html/pages/recipe.html', 'utf8'));
 const recipeTemplate = Handlebars.compile(fs.readFileSync('html/layouts/default.hbs', 'utf8'));
 
-const data = Yaml.load('html/data/recipes.yml');
-
-const recipeHtml = recipeTemplate(data[0]);
-fs.writeFileSync('public/recipe.html', recipeHtml);
+recipeData.forEach(recipe => {
+  const recipeHtml = recipeTemplate(recipe);
+  fs.writeFileSync(`public/recipe/${recipe.urlName}.html`, recipeHtml);
+});
