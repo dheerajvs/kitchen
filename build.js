8ecdf6cb7ac24d4d22a05d0fa2d7d80ceafbc402
Dheeraj <dheerajvs@gmail.com>
2017-05-12 05:49:36 +0530

    chore: use cpx to copy from src to public and watch

--- build.js
+++ build.js
@@ -5,7 +5,6 @@ const Handlebars = require('handlebars');
 const duration = require('./html/helpers/duration');
 const localeDate = require('./html/helpers/localeDate');
 const recipeData = Yaml.load('html/data/recipes.yml');
-// fs.mkdirSync('public/recipe');
 
 Handlebars.registerHelper('duration', duration);
 Handlebars.registerHelper('localeDate', localeDate);
@@ -16,6 +15,10 @@ fs.writeFileSync('public/index.html', indexTemplate(recipeData));
 
 Handlebars.registerPartial('body', fs.readFileSync('html/pages/recipe.html', 'utf8'));
 const recipeTemplate = Handlebars.compile(fs.readFileSync('html/layouts/default.hbs', 'utf8'));
+
+if (!fs.existsSync('public/recipe')) {
+  fs.mkdirSync('public/recipe');
+}
 
 recipeData.forEach(recipe => {
   const recipeHtml = recipeTemplate(recipe);
