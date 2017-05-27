6664900168831541a079bd917e43831e0c49e006
Dheeraj <dheerajvs@gmail.com>
2017-05-27 19:38:14 +0530

    two-line ingredients; category page

--- build.js
+++ build.js
@@ -22,14 +22,16 @@ require('handlebars-helpers')({handlebars: Handlebars});
 Handlebars.registerHelper('duration', duration);
 Handlebars.registerHelper('urlName', urlName);
 
+const defaultTemplate = fs.readFileSync('html/layouts/default.hbs', 'utf8');
+
 // Generate /index.html
 Handlebars.registerPartial('body', fs.readFileSync('html/pages/index.html', 'utf8'));
-const indexTemplate = Handlebars.compile(fs.readFileSync('html/layouts/default.hbs', 'utf8'));
+const indexTemplate = Handlebars.compile(defaultTemplate);
 fs.writeFileSync('public/index.html', indexTemplate({baseUrl: '', recipes}));
 
 // Generate /recipe/:recipe.html
 Handlebars.registerPartial('body', fs.readFileSync('html/pages/recipe.html', 'utf8'));
-const recipeTemplate = Handlebars.compile(fs.readFileSync('html/layouts/default.hbs', 'utf8'));
+const recipeTemplate = Handlebars.compile(defaultTemplate);
 
 if (!fs.existsSync('public/recipe')) {
   fs.mkdirSync('public/recipe');
@@ -39,5 +41,10 @@ recipes.forEach(recipe => {
   const recipeHtml = recipeTemplate({baseUrl: '../', recipe});
   fs.writeFileSync(`public/recipe/${urlName(recipe.title)}.html`, recipeHtml);
 });
+
+// Generate /categories.html
+Handlebars.registerPartial('body', fs.readFileSync('html/pages/categories.html', 'utf8'));
+const categoriesTemplate = Handlebars.compile(defaultTemplate);
+fs.writeFileSync('public/categories.html', categoriesTemplate({baseUrl: ''}));
 
 console.log('build: completed');
